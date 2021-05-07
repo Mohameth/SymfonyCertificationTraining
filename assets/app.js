9621b3bb494b70fbcd3ce857d4fd0722c859a4c7
Mohameth <mohameth@wiztopic.com>
2021-05-07 16:14:28 +0200

    Encore & JQuery

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,22 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+// loads the jquery package from node_modules
+import jQuery from 'jquery';
+
+// require jQuery normally
+const $ = require('jquery');
+
+// create global $ and jQuery variables
+global.$ = global.jQuery = $;
+
+// import the function from greet.js (the .js extension is optional)
+// ./ (or ../) means to look for a local file
+import navHandler from './navHandler';
+
+
+
+$(document).ready(function() {
+  $('nav > ol').append('<li>'+navHandler()+'</li>');
+});
